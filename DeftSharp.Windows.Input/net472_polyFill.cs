26b2cd898a2b8deeb3c43a9b4ab6daf19f52287a
blackholeearth <blackholeearth@users.noreply.github.com>
2025-03-03 22:25:30 +0300

    support .net 4.7.2

---
+++ DeftSharp.Windows.Input/net472_polyFill.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+//namespace DeftSharp.Windows.Input{ }
+
+/// <summary>
+///  net472_polyFill
+/// </summary>
+public static partial class PolyFillExtensions
+{
+    //conditinal compile to get best performance.. 
+    //when testing Remove conditinal formatting.
+#if NETFRAMEWORK
+    /// <summary>
+    /// net472_polyFill - Queue.TakeLast
+    /// </summary>
+    public static IEnumerable<T> TakeLast<T>(this Queue<T> queue, int count)
+    {
+        if (queue is null)
+            throw new ArgumentNullException("source is null ");
+
+        if (count <= 0 || queue.Count == 0)
+            return [];
+
+        if (count <= 0)
+            return Enumerable.Empty<T>();
+        if (count >= queue.Count)
+            return queue.ToList();
+
+        //  isStartIndexFromEnd: true, startIndex: count,
+        //  isEndIndexFromEnd: true, endIndex: 0);
+        //return queue.Take(queue.Count - count).ToList();
+
+        var startIndex = queue.Count - count;
+        var take = count;
+
+        return queue.Skip(startIndex).Take(take).ToList();
+
+    }
+
+
+    /// <summary>
+    ///  net472_polyFill
+    /// </summary>
+    public static TValue GetValueOrDefault<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+    {
+        return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+    }
+
+    /// <summary>
+    ///  net472_polyFill 
+    /// </summary>
+    public static bool TryRemove<TK, TValue>(this ConcurrentDictionary<TK, TValue> dictionary, KeyValuePair<TK, TValue> item)
+    {
+        return dictionary.TryRemove(item.Key, out _);
+    }
+#endif
+
+
+    /// <summary>
+    /// net472_polyFill - nint.Zero
+    /// </summary>
+    public static nint nint_Zero
+    {
+        get { return (nint)0; } // Or return default(nint); in .NET Framework 4.7.2
+    }
+
+}
+
+ 
+
---
+++ Test_polyfill/PolyFillTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using System.Linq;
+using Xunit;
+
+namespace DeftSharp.Windows.Input;
+
+public sealed class PolyFillTest
+{
+
+    [Fact]
+    public async void TakeLast_1()
+    {
+        int TakeLast_count = 1;
+        var que = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+
+        var val = Enumerable.TakeLast(que, TakeLast_count).ToList();  // net8 original Linq
+        var valpf = PolyFillExtensions.TakeLast(que, TakeLast_count).ToList();
+
+        Assert.True(val[0] == valpf[0]);
+        //await Task.Run(() => { });
+    }
+
+    [Fact]
+    public async void TakeLast_2()
+    {
+        int TakeLast_count = 2;
+        var que = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+
+        var val = Enumerable.TakeLast(que, TakeLast_count).ToList();  // net8 original Linq
+        var valpf = PolyFillExtensions.TakeLast(que, TakeLast_count).ToList();
+
+        Assert.True(val[0] == valpf[0]);
+        Assert.True(val[1] == valpf[1]);
+    }
+
+    [Fact]
+    public async void TakeLast_3()
+    {
+        int TakeLast_count = 5;
+        var que = new Queue<int>(new[] { 1, 2, 3, 4, 5 });
+        
+        var val = Enumerable.TakeLast(que, TakeLast_count).ToList();  // net8 original Linq
+        var valpf = PolyFillExtensions.TakeLast(que, TakeLast_count).ToList();
+
+        Assert.True(val[0] == valpf[0]);
+        Assert.True(val[4] == valpf[4]);
+    }
+
+
+}
